3658e77ff5026b319911f7405ed34327c926d790
Ruddy Cruz <rudolph555.dev>
2024-05-04 12:51:31 -0400

    ejercicio 02 sesion 03

---
+++ retos_sesion_03/ejercicio_02.py
@@ -0,0 +1,15 @@
+print("Camarero: Buenas tardes, señorita ¿en qué puedo servirles?")
+
+print("Cliente: Buenas tardes ¿puede decirme que tiene la hamburguesa regular?")
+
+print("Camarero: Por supuesto, tiene 200 gr de carne, queso cheddar, tocino, rodajas de tomate y salsas.")
+
+print("Cliente: Excelente. Quiero una, acompañada de una gaseosa.")
+
+print("Camarero: Muy bien. ¿Querrá algún postre con la comida?")
+
+print("Cliente: Aún no lo sé. Te diré cuando traigas la hamburguesa.")
+
+print("Camarero: De acuerdo. En un momento le traeré su pedido.")
+
+print("Cliente: Muchas gracias.")
